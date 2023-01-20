d6a1fcb93c9075725f4cd2cd60b1e93ee1850c56
jeremdh62 <jdhaille@myges.fr>
2023-01-20 12:15:53 +0100

    add restriction routing

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 import RegisterView from "../views/RegisterView.vue";
+//import store from "../store";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,10 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      /*redirect:
+        store.state.user.id == "" || store.state.user.token == ""
+          ? "/login"
+          : "",*/
     },
     {
       path: "/login",
@@ -20,6 +25,10 @@ const router = createRouter({
       path: "/register",
       name: "register",
       component: RegisterView,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      redirect: "/",
     },
   ],
 });
