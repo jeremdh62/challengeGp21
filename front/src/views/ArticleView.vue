a5d0bc1572d281929d8686871d8e2af5021583bb
jeremdh62 <jdhaille@myges.fr>
2023-02-09 20:08:30 +0100

    modify article view

--- front/src/views/ArticleView.vue
+++ front/src/views/ArticleView.vue
@@ -1,20 +1,21 @@
 <template>
-  <div>
+  <v-container>
     <v-btn text color="teal accent-4" @click="navigate('articles')">
       Back
     </v-btn>
-    <v-card variant="tonal">
+    <v-card class="text-center">
       <v-card-title>
         <div>
-          <h3 class="headline mb-0">{{ article.title }}</h3>
+          <h3 class="text-h3 mb-0">{{ article.title }}</h3>
           <div>{{ article.author }}</div>
+          <v-divider></v-divider>
         </div>
       </v-card-title>
       <v-card-text>
         <div v-html="article.content"></div>
       </v-card-text>
     </v-card>
-  </div>
+  </v-container>
 </template>
 <script>
 import { mapGetters } from "vuex";
